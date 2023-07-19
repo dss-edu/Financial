345715801908d955bc5b6b504cd8db812d0b6909
earlestradalopez <61444155+earlestradalopez@users.noreply.github.com>
2023-07-19 22:21:47 +0800

    Update urls.py
    add url for pl cumberland and advantage

--- config/finance/urls.py
+++ config/finance/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('login/',views.loginView,name = 'login'),
     path('login_form/',views.login_form,name = 'login_form'),
     path('logout/', views.logoutView, name='logout'),
-    path('dashboard/',views.dashboard,name = 'dashboard'),
+    path('pl_advantage/',views.pl_advantage,name = 'pl_advantage'),
+    path('pl_cumberland/',views.pl_cumberland,name = 'pl_cumberland'),
+    path('insert_row/', views.insert_row, name='insert-row'),
+    
 ]
