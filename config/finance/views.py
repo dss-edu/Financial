1f4f7cd8fb2b80e63b9393f3c75ba2246473055d
earlestradalopez <61444155+earlestradalopez@users.noreply.github.com>
2023-07-21 08:31:01 +0800

    Update views.py

--- config/finance/views.py
+++ config/finance/views.py
@@ -28,7 +28,7 @@ def connect():
     password = 'Pokemon!123'
     port = '1433'
     
-    driver = '{SQL Server}'
+    driver = '{/usr/lib/libmsodbcsql-17.so}'
 
     cnxn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
     return cnxn
