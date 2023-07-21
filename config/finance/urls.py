cbb17196e72c81ef4084803eb48e6821175685e6
earlestradalopez <61444155+earlestradalopez@users.noreply.github.com>
2023-07-21 18:34:54 +0800

    Update urls.py

--- config/finance/urls.py
+++ config/finance/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('pl_advantage/',views.pl_advantage,name = 'pl_advantage'),
     path('pl_cumberland/',views.pl_cumberland,name = 'pl_cumberland'),
     path('insert_row/', views.insert_row, name='insert-row'),
+    path('delete/<str:fund>/<str:obj>/', views.delete, name='delete'),
     
 ]
