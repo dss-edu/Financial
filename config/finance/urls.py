ed6cb240cc960b1846f7c6e5e73effef7d1f391a
earlestradalopez <61444155+earlestradalopez@users.noreply.github.com>
2023-07-21 08:23:37 +0800

    Update urls.py

--- config/finance/urls.py
+++ config/finance/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload/', views.upload_view, name='upload'),
     path('login/',views.loginView,name = 'login'),
     path('login_form/',views.login_form,name = 'login_form'),
     path('logout/', views.logoutView, name='logout'),
