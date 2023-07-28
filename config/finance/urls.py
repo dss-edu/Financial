bff34954080f326a5c1c12d3aed37c1a4e553068
earlestradalopez <61444155+earlestradalopez@users.noreply.github.com>
2023-07-28 17:05:27 +0800

    Update urls.py

--- config/finance/urls.py
+++ config/finance/urls.py
@@ -6,11 +6,18 @@ urlpatterns = [
     path('login_form/',views.login_form,name = 'login_form'),
     path('logout/', views.logoutView, name='logout'),
     path('pl_advantage/',views.pl_advantage,name = 'pl_advantage'),
-    path('pl_cumberland/',views.pl_cumberland,name = 'pl_cumberland'),
+    path('pl_advantagechart/', views.pl_advantagechart, name='pl_advantagechart'),
+    path('gl_advantage/', views.gl_advantage, name='gl_advantage'),
+    
     path('insert_row/', views.insert_row, name='insert-row'),
     path('delete/<str:fund>/<str:obj>/', views.delete, name='delete'),
-    path('pl_advantagechart/', views.pl_advantagechart, name='pl_advantagechart'),
-    path('pl_cumberlandchart/', views.pl_cumberlandchart, name='pl_cumberlandchart'),
+    
+    
     path('delete_func/<str:func>/', views.delete_func, name='delete_func'),
+    path('viewgl/<str:fund>/<str:obj>/<str:yr>/', views.viewgl, name='viewgl'),
+
+    
+    path('pl_cumberlandchart/', views.pl_cumberlandchart, name='pl_cumberlandchart'),
+    path('pl_cumberland/',views.pl_cumberland,name = 'pl_cumberland'),
     
 ]
