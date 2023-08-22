5e4de1ebbd048d83be326a02f304136a4558ae91
earlestradalopez <61444155+earlestradalopez@users.noreply.github.com>
2023-08-22 19:12:30 +0800

    Update urls.py

--- config/finance/urls.py
+++ config/finance/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('bs_advantage/', views.bs_advantage, name='bs_advantage'),
     path('cashflow_advantage/', views.cashflow_advantage, name='cashflow_advantage'),
     path('first_advantage/', views.first_advantage, name='first_advantage'),
+    path('generate_excel/', views.generate_excel, name='generate_excel'),
 
     
     path('insert_row/', views.insert_row, name='insert-row'),
