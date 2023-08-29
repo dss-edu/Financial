393ffc89d4fa463d484e6120ec5cd4faf0fcc06b
FaRm3rKing <re.georgejrnm@protonmail.com>
2023-08-30 00:27:27 +0800

    loading spinner on page navigation

---
+++ config/finance/static/js/loading.js
@@ -0,0 +1,20 @@
+$('#page-load-spinner').modal('show');  // Close the modal
+
+window.addEventListener('load', function() {
+  $('#page-load-spinner').modal('hide');  // Close the modal
+});
+
+$(document).ready(function() {
+  const sidebarWrapper = document.querySelector('.sidebar');
+  const linkEl = sidebarWrapper.querySelectorAll('li a');
+
+
+  linkEl.forEach(link => {
+    console.log(link);
+    link.addEventListener('click', function() {
+      $('#page-load-spinner').modal('show');  // Close the modal
+    });
+  });
+  // $('#page-load-spinner').modal('show');  // Close the modal
+});
+
