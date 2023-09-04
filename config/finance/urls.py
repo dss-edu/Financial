3f29b0c8018e9f356d6ab10594e21f096ddfc57a
su3zzz <73903886+su3zzz@users.noreply.github.com>
2023-09-04 19:33:50 +0800

    Update urls.py

--- config/finance/urls.py
+++ config/finance/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     # path('gl_advantage/', views.gl_advantage, name='gl_advantage'),
     # path('bs_advantage/', views.bs_advantage, name='bs_advantage'),
     # path('cashflow_advantage/', views.cashflow_advantage, name='cashflow_advantage'),
-    # path('generate_excel/', views.generate_excel, name='generate_excel'),
+    
     # path('dashboard_advantage/', views.dashboard_advantage, name='dashboard_advantage'),
 
     
@@ -28,6 +28,7 @@ urlpatterns = [
     path('insert_bs_advantage/', views.insert_bs_advantage, name='insert_bs_advantage'),
     path('delete_bs/<str:description>/<str:subcategory>/', views.delete_bs, name='delete_bs'),
     path('delete_bsa/<str:obj>/<str:Activity>/', views.delete_bsa, name='delete_bsa'),
+    path('generate_excel/', views.generate_excel, name='generate_excel'),
     
   
     path('delete_func/<str:func>/', views.delete_func, name='delete_func'),
