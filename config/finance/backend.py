4a01a2677d32037ad27bcf98eaa0db394d226a4e
earlestradalopez <61444155+earlestradalopez@users.noreply.github.com>
2023-10-10 20:43:37 +0800

    Update backend.py

--- config/finance/backend.py
+++ config/finance/backend.py
@@ -4785,14 +4785,13 @@ def profit_loss_chart(school):
     for item in data3:
         date_str = item["Date"]
         if date_str:
-            if school == 'manara' or school == 'leadership':
-               
-                date_obj = datetime.strptime(date_str, "%Y-%m-%d").date()
+            if school == 'manara' or school == 'leadership:
+                date_obj = datetime.strptime(str(date_str), "%Y-%m-%d").date()
                 if date_obj > july_date: # if date is higher than july 1 this year
                   FY_year_1 = current_year
                   FY_year_2 = next_year
             else:
-                date_obj = datetime.strptime(date_str, "%Y-%m-%d").date()
+                date_obj = datetime.strptime(str(date_str), "%Y-%m-%d").date()
                 if date_obj > september_date: # if date is higher than july 1 this year
                   FY_year_1 = current_year
                   FY_year_2 = next_year
