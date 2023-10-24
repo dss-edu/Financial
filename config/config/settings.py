a8519a172aedce09cf85af435d5a2a43835cae73
earlestradalopez <61444155+earlestradalopez@users.noreply.github.com>
2023-10-24 13:59:28 +0800

    Update settings.py

--- config/config/settings.py
+++ config/config/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = [
     "ec2-35-77-85-255.ap-northeast-1.compute.amazonaws.com",
     "ec2-54-199-141-147.ap-northeast-1.compute.amazonaws.com",
     "ec2-35-78-202-130.ap-northeast-1.compute.amazonaws.com",
+    "ec2-13-230-82-195.ap-northeast-1.compute.amazonaws.com",
+    "data.dssedu.com",
     "financialreport.dssedu.com",
     "localhost"
 ]
