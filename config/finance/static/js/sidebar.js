c7ea761a523eb1b783f4cedb8dc6566970205178
zeus-lee <zeuslee612@gmail.com>
2023-11-09 13:52:38 +0800

    sidebar highlight

--- config/finance/static/js/sidebar.js
+++ config/finance/static/js/sidebar.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const school = $("#hidden-school").val();
     const year = $("#hidden-year").val();
+    const currentPath = window.location.pathname;
     // /charter-first/{{ school }}
+    function highlightActiveLink(linkId) {
+        $(linkId).addClass("activebg"); // Add a class for styling
+    }
+
     $("#first-link").on("click", function(event) {
         event.preventDefault();
 
@@ -56,4 +61,23 @@ document.addEventListener("DOMContentLoaded", function() {
             window.location.href = "/general-ledger/" + school;
         }
     });
+
+    if (currentPath === "/charter-first/" + school || currentPath === "/charter-first/" + school + "/" + year) {
+        highlightActiveLink("#first-link");
+        highlightActiveLink("#first-link2");
+    
+    } else if (currentPath === "/profit-loss/" + school || currentPath === "/profit-loss/" + school + "/" + year) {
+        highlightActiveLink("#pl-link");
+        highlightActiveLink("#pl-link2");
+    } else if (currentPath === "/balance-sheet/" + school || currentPath === "/balance-sheet/" + school + "/" + year) {
+        highlightActiveLink("#bs-link");
+        highlightActiveLink("#bs-link2");
+    } else if (currentPath === "/cashflow-statement/" + school || currentPath === "/cashflow-statement/" + school + "/" + year) {
+        highlightActiveLink("#cs-link");
+        highlightActiveLink("#cs-link2");
+    } else if (currentPath === "/general-ledger/" + school || currentPath === "/general-ledger/" + school + "/" + year) {
+        highlightActiveLink("#gl-link");
+        highlightActiveLink("#gl-link2");
+    }
+
 });
