7fb5518d27b54e06f2c00ad002e32ed5270aecc3
FaRm3rKing <re.georgejrnm@protonmail.com>
2023-11-16 15:06:45 +0800

    wsgi

--- config/config/wsgi.py
+++ config/config/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
+os.environ['WSGIApplicationGroup'] = '%%{GLOBAL}'
 application = get_wsgi_application()
