277a8a1342c0235c7673bfd54391b300286b59bc
George Morales <133523465+FaRm3rKing@users.noreply.github.com>
2023-12-08 05:10:58 +0800

    Update update_db.sh

--- config/update_db.sh
+++ config/update_db.sh
@@ -2,3 +2,5 @@
 source /var/www/Financial/config/venv/bin/activate
 python /var/www/Financial/config/update_db.py
 deactivate
+chown -R www-data:www-data /var/www/Financial/config/finance/json/
+chmod -R 775 /var/www/Financial/config/finance/json/
