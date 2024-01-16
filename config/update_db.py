1a904a98dba0394e1fec6b42a08a4e64715ccf21
earlestradalopez <61444155+earlestradalopez@users.noreply.github.com>
2024-01-16 11:45:09 +0800

    Update update_db.py

--- config/update_db.py
+++ config/update_db.py
@@ -1,10 +1,10 @@
-# from finance.connect import connect
+from finance.connect import connect
 from finance.backend import update_fy
 from config.settings import SCHOOLS
 
 if __name__ == "__main__":
-    for school in SCHOOLS.keys():
-        update_fy(school, '2024')
+    #for school in SCHOOLS.keys():
+    #    update_fy(school, '2024')
 
     # --- UPDATE ASCENDER CLIENTS ---
     cnxn = connect()
