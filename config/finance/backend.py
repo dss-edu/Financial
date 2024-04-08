139fc73c1dd50e2e093a6ca8e7e7f43a62e84d0b
zeus-lee <zeus_lee@trendmicro.com>
2024-04-08 14:13:35 +0800

    pl

--- config/finance/backend.py
+++ config/finance/backend.py
@@ -55,20 +55,20 @@ def update_school(school):
     profit_loss_date(school)  # 
 
 def update_fy(school,year):    
-    writeCodes(school, db[school]['db'], year)
-    updateDescription(db[school]['db'], school)
+    # writeCodes(school, db[school]['db'], year)
+    # updateDescription(db[school]['db'], school)
     profit_loss(school,year) 
-    balance_sheet(school,year)
-    cashflow(school,year)
-    charter_first(school)
-    updateGraphDB(school, True)
-    profit_loss_chart(school)
-    profit_loss_date(school)
-    excel(school,year)
-    if school in schoolCategory["ascender"]:
-        balance_sheet_asc(school,year)        
-    school_status(school)
-    run_all_monthly(school,year)
+    # balance_sheet(school,year)
+    # cashflow(school,year)
+    # charter_first(school)
+    # updateGraphDB(school, True)
+    # profit_loss_chart(school)
+    # profit_loss_date(school)
+    # excel(school,year)
+    # if school in schoolCategory["ascender"]:
+    #     balance_sheet_asc(school,year)        
+    # school_status(school)
+    # run_all_monthly(school,year)
     
 
 
@@ -790,6 +790,7 @@ def profit_loss(school,year):
                     
                 if category == 'State Program Revenue':
                     total_spr[acct_per] += (item[f"total_real{i}"])
+                    print("SPR", acct_per,total_spr[acct_per])
                     if i != month_exception:
                         ytd_total_spr += (item[f"total_real{i}"])
                     
