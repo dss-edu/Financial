897f82d5c7ccaaf1b1a13ec6efb2d0c513630109
zeus-lee <zeus_lee@trendmicro.com>
2024-05-15 14:19:41 +0800

    backend

--- config/finance/backend.py
+++ config/finance/backend.py
@@ -57,19 +57,19 @@ def update_school(school):
     run_all_monthly_fy(school)
 
 def update_fy(school,year):    
-    # writeCodes(school, db[school]['db'], year)
-    # updateDescription(db[school]['db'], school)
-    # profit_loss(school,year) 
+    writeCodes(school, db[school]['db'], year)
+    updateDescription(db[school]['db'], school)
+    profit_loss(school,year) 
     balance_sheet(school,year)
-    # cashflow(school,year)
-    # charter_first(school)
-    # updateGraphDB(school, True)
-    # profit_loss_chart(school)
-    # profit_loss_date(school)
-    # if school in schoolCategory["ascender"]:
-    #     balance_sheet_asc(school,year)        
-    # school_status(school)
-    # run_all_monthly(school,year)
+    cashflow(school,year)
+    charter_first(school)
+    updateGraphDB(school, True)
+    profit_loss_chart(school)
+    profit_loss_date(school)
+    if school in schoolCategory["ascender"]:
+        balance_sheet_asc(school,year)        
+    school_status(school)
+    run_all_monthly(school,year)
 
     print("DONE UPDATING")
     # excel(school,year)
