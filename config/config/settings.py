d32b532187c8917521fe3b8cff4be32825c09fa4
earlestradalopez <61444155+earlestradalopez@users.noreply.github.com>
2024-05-21 11:17:48 +0800

    Update settings.py

--- config/config/settings.py
+++ config/config/settings.py
@@ -550,7 +550,7 @@ schoolMonths ={
     "septemberSchool" : ["advantage","cumberland","village-tech","legacy", "cityscape", "ptaa", "aca", "trivium", "pro-vision", "ume", "stmary", "goldenrule", "eastfortworthmontessori", "aacademy", "inspiredvision", "trinitybasin", "winfree", "aama", "ptaa-colorado"]
 }
 
-school_fye = ['aca','advantage','cumberland','pro-vision','manara','stmary','sa','aacademy', "aacademy", "inspiredvision", "aama"]
+school_fye = ['aca','advantage','cumberland','pro-vision','manara','stmary','sa','aacademy', "aacademy", "inspiredvision", "aama", "prelude"]
 
 
 # filters must be a dictionary with key = column and value = "(string of values to filter)"
